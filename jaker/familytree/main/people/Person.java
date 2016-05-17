d55291cdabc3b8b12b3ac9857ea6b00f2df95da1
jjjhfam <jchelmert3@yahoo.com>
2016-05-17 18:49:31 -0500

    Added Several Comments

--- jaker/familytree/main/people/Person.java
+++ jaker/familytree/main/people/Person.java
@@ -14,19 +14,27 @@ public class Person {
 	private ArrayList<Male> sons = new ArrayList<Male>();
 	private ArrayList<Female> daughters = new ArrayList<Female>();
 	private ArrayList<Person> spouses = new ArrayList<Person>();
+
+	private int generation;
+	private static int numberOfPeople = 0;
+	// Constructors are based on names
 	
-	public Person() {}
+	public Person() {
+		numberOfPeople++;
+	}
 	
 	public Person(final String firstName, final String lastName) {
+		this();
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
 	
 	public Person(final String firstName, final String middleName, final String lastName) {
-		this.firstName = firstName;
+		this(firstName, lastName);
 		this.middleName = middleName;
-		this.lastName = lastName;
 	}
+	
+	// Set parents
 	
 	public void setFather(final Male father) {
 		this.father = father;
@@ -41,13 +49,17 @@ public class Person {
 		setMother(mother);
 	}
 	
+	// Add children
+	
 	public void addSon(final Male son) {
-		sons.add(son);
+		this.sons.add(son);
 	}
 	
 	public void addDaughter(final Female daughter) {
-		daughters.add(daughter);
+		this.daughters.add(daughter);
 	}
+	
+	// Add spouse(s)
 	
 	public void addSpouse(final Person spouse) {
 		this.spouses.add(spouse);
